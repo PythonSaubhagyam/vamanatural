2d1e4223ff81827980c57fb43c9752a08e1606c9
princesaubhagyam <prince@saubhagyam.org>
2024-12-31 14:31:50 +0530

    fix: navbar error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -604,7 +604,7 @@ export default function Navbar() {
                     _hover={{ bg: "brand.500" }}
                   > */}
                   <MenuItem
-                    onClick={()=>setIsLoginModalOpen(true)}
+                    onClick={() => setIsLoginModalOpen(true)}
                     cursor={"pointer"}
                     _hover={{ textDecoration: "none" }}
                   >
@@ -1095,7 +1095,7 @@ export default function Navbar() {
                     }}
                     fontWeight={500}
                     fontSize={{ md: "14px" }}
-                    onClick={()=>setIsLoginModalOpen(true)}
+                    onClick={() => setIsLoginModalOpen(true)}
                   >
                     Login
                   </Link>
@@ -1161,13 +1161,13 @@ export default function Navbar() {
                     {megaCategories?.map((section, index) => (
                       <>
                         <MenuItem
-                          icon={
-                            <img
-                              src={"./himalayan_logo.jpg"}
-                              width={25}
-                              alt=""
-                            />
-                          }
+                          // icon={
+                          //   <img
+                          //     src={"./himalayan_logo.jpg"}
+                          //     width={25}
+                          //     alt=""
+                          //   />
+                          // }
                           fontSize={"14"}
                           key={index}
                           onMouseEnter={() => handleShow1(section.children)}
